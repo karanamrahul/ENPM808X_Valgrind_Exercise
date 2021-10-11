a04e44d107df9a97b6cc2a586083a275d0979bdb
karanamrahul <rahulkarnam777@gmail.com>
2021-10-11 11:59:33 -0400

    Made changes to AnalogSensor.cpp to remove memory leak errors

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -13,8 +13,10 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
-
+   // std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+   /* created a new vector which has been initalized and
+      removed new because it creates memory leakage to a uninitalized vector */
+    std::vector<int> readings(mSamples,10);
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
 }
